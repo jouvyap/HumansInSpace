823ccb0feee3ea5fd1ece141203297fee03d5b82
FLP-9-Jouvy <flp-9-jouvy@IT-Admins-MacBook-Pro-2.local>
2023-01-08 01:23:57 +0700

    animate earth and astronauts, add fab and scaffold in main screen

--- app/src/main/java/com/bravostudio/humansinspace/MainActivity.kt
+++ app/src/main/java/com/bravostudio/humansinspace/MainActivity.kt
@@ -3,13 +3,28 @@ package com.bravostudio.humansinspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bravostudio.humansinspace.ui.theme.HumansInSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,22 +35,100 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             HumansInSpaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = viewModel()) {
     val astronautCounts = viewModel.astronautCounts.value
     viewModel.getAstronauts()
 
-    Text(text = "Humans in Space: $astronautCounts")
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val rotationAstronaut by infiniteTransition.animateFloat(
+        initialValue = -20f,
+        targetValue = 20f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(5000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val rotationEarth by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(20000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Humans in Space",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                content = {
+                    Icon(Icons.Filled.List, contentDescription = "list button")
+                }
+            )
+        }
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
+            Image(
+                painter = painterResource(id = R.drawable.earth),
+                contentDescription = "earth image",
+                modifier = Modifier
+                    .rotate(rotationEarth)
+                    .align(Alignment.Center)
+                    .size(300.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.astronaut),
+                contentDescription = "astronaut image",
+                modifier = Modifier
+                    .scale(scale)
+                    .rotate(rotationAstronaut)
+                    .align(Alignment.TopEnd)
+            )
+
+            if (astronautCounts > 0) {
+                Text(
+                    text = "" + astronautCounts,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+
 }
