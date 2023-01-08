414b0229246f18933abee36f081514677f2e98c4
FLP-9-Jouvy <flp-9-jouvy@IT-Admins-MacBook-Pro-2.local>
2023-01-08 11:38:27 +0700

    add theme color and tidy up components

--- app/src/main/java/com/bravostudio/humansinspace/MainActivity.kt
+++ app/src/main/java/com/bravostudio/humansinspace/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,6 +32,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bravostudio.humansinspace.ui.theme.HumansInSpaceTheme
 import kotlinx.coroutines.launch
+import java.lang.Math.random
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,35 +59,11 @@ fun MainScreen(viewModel: MainViewModel = viewModel()) {
     val scope = rememberCoroutineScope()
     val infiniteTransitionScaAst = rememberInfiniteTransition()
     val infiniteTransitionRotAst = rememberInfiniteTransition()
-    val infiniteTransitionRotEar = rememberInfiniteTransition()
-    val modalBottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val modalBottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden, skipHalfExpanded = true)
 
-    val scale by infiniteTransitionScaAst.animateFloat(
-        initialValue = 1f,
-        targetValue = 1.2f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(3000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
 
-    val rotationAstronaut by infiniteTransitionRotAst.animateFloat(
-        initialValue = -20f,
-        targetValue = 20f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(5000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
 
-    val rotationEarth by infiniteTransitionRotEar.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(20000, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        )
-    )
+
 
     ModalBottomSheetLayout(
         sheetContent = { BottomSheetContent(astronautList) },
@@ -119,24 +98,18 @@ fun MainScreen(viewModel: MainViewModel = viewModel()) {
         ) {
             Box(modifier = Modifier
                 .fillMaxSize()
-                .padding(it)) {
-                Image(
-                    painter = painterResource(id = R.drawable.earth),
-                    contentDescription = "earth image",
-                    modifier = Modifier
-                        .rotate(rotationEarth)
-                        .align(Alignment.Center)
-                        .size(300.dp)
-                )
+                .padding(it)
+                .background(Color.Black)
+            ) {
 
-                Image(
-                    painter = painterResource(id = R.drawable.astronaut),
-                    contentDescription = "astronaut image",
-                    modifier = Modifier
-                        .scale(scale)
-                        .rotate(rotationAstronaut)
-                        .align(Alignment.TopEnd)
-                )
+                CreateEarth(modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(300.dp))
+
+                CreateAstronaut(modifier = Modifier.align(Alignment.TopCenter))
+                CreateAstronaut(modifier = Modifier.align(Alignment.TopStart))
+                CreateAstronaut(modifier = Modifier.align(Alignment.TopEnd))
+
 
                 if (astronautCount > 0) {
                     Text(
@@ -153,12 +126,78 @@ fun MainScreen(viewModel: MainViewModel = viewModel()) {
 }
 
 @Composable
+fun CreateEarth(modifier: Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val rotationEarth by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(30000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Image(
+        painter = painterResource(id = R.drawable.earth),
+        contentDescription = "earth image",
+        modifier = modifier.rotate(rotationEarth)
+    )
+}
+
+@Composable
+fun CreateAstronaut(modifier: Modifier) {
+    val infiniteTransitionScale = rememberInfiniteTransition()
+    val infiniteTransitionRotation = rememberInfiniteTransition()
+    val scale by infiniteTransitionScale.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(Random.nextInt(2000, 4000)),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val rotationAstronaut by infiniteTransitionRotation.animateFloat(
+        initialValue = -20f,
+        targetValue = 20f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(Random.nextInt(3000, 5000)),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Image(
+        painter = painterResource(id = R.drawable.astronaut),
+        contentDescription = "astronaut image",
+        modifier = modifier
+            .scale(scale)
+            .rotate(rotationAstronaut)
+    )
+}
+
+@Composable
 private fun BottomSheetContent(astronautList: MutableList<Astronaut>) {
     Column(modifier = Modifier.padding(8.dp)) {
         Text(text = "List of Humans", fontSize = 18.sp, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
         Divider(modifier = Modifier.padding(8.dp))
-        astronautList.forEachIndexed { _, ast ->
-            Text(text = ast.name + " - " + ast.craft)
+        astronautList.forEachIndexed { i, ast ->
+            BottomSheetListItem(numberText = i + 1, name = ast.name, craft = ast.craft)
         }
+    }
+}
+
+@Composable
+private fun BottomSheetListItem(numberText: Int, name: String, craft: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "" + numberText)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = name)
+        Text(text = craft, modifier = Modifier.weight(1f), textAlign = TextAlign.End)
     }
 }
